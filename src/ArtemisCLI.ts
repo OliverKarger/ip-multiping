2e5e887df618790905578b5d40340bbd6fb1ec8b
Oliver Karger <kmaster@oliver-karger.de>
2021-04-13 21:25:39 +0200

    Added prettier Script to NPM and implemented ESLint
    Signed-off-by: Oliver Karger <kmaster@oliver-karger.de>

--- src/ArtemisCLI.ts
+++ src/ArtemisCLI.ts
@@ -1,7 +1,7 @@
-import chalk from "chalk";
-import { circleQuestionMark } from "figures";
-import boxen from "boxen";
-import logSymbols from "log-symbols";
+import chalk from 'chalk';
+import { circleQuestionMark } from 'figures';
+import boxen from 'boxen';
+import logSymbols from 'log-symbols';
 
 // REUSED CONSTS
 export const IPMultiPingAsciiLogo = `
@@ -12,20 +12,21 @@ export const IPMultiPingAsciiLogo = `
 |___||____|     \\____|__  /____/|____/__| |__|   __/|__|___|  /\___  / 
                         \\/                   |__|           \\//_____/  `;
 
-export const lineBreak = "-------------------------------";
+export const lineBreak = '-------------------------------';
 
 /**
  * @author Oliver Karger
  * @version 1.0
  * @description Prints 'ArtemisCLI' to Console using npm's boxes
  */
-export function Header() {
+export function header() {
+    'use strict';
     console.log(
         boxen(IPMultiPingAsciiLogo, {
             padding: 1,
             margin: 1,
-            borderStyle: "round",
-            borderColor: "green",
+            borderStyle: 'round',
+            borderColor: 'green',
         })
     );
 }
@@ -35,8 +36,9 @@ export function Header() {
  * @description Displays Custom Error Message to Console
  * @param msg The Error Message to be displayed
  */
-export function WriteError(msg: string) {
-    console.log(logSymbols.error, chalk.redBright.bold("[ ERR! ]"), msg);
+export function writeError(msg: string) {
+    'use strict';
+    console.log(logSymbols.error, chalk.redBright.bold('[ ERR! ]'), msg);
 }
 
 /**
@@ -44,8 +46,9 @@ export function WriteError(msg: string) {
  * @description Displays Custom Warning Message to Console
  * @param msg The Warning Message to be displayed
  */
-export function WriteWarning(msg: string) {
-    console.log(logSymbols.warning, chalk.yellowBright.bold("[ WARN ]"), msg);
+export function writeWarning(msg: string) {
+    'use strict';
+    console.log(logSymbols.warning, chalk.yellowBright.bold('[ WARN ]'), msg);
 }
 
 /**
@@ -53,8 +56,9 @@ export function WriteWarning(msg: string) {
  * @description Displays Custom Info Message to Console
  * @param msg The Informational Message to be displayed
  */
-export function WriteInfo(msg: string) {
-    console.log(logSymbols.info, chalk.cyanBright.bold("[ INFO ]"), msg);
+export function writeInfo(msg: string) {
+    'use strict';
+    console.log(logSymbols.info, chalk.cyanBright.bold('[ INFO ]'), msg);
 }
 
 /**
@@ -62,8 +66,9 @@ export function WriteInfo(msg: string) {
  * @description Displays Custom Success Message to Console
  * @param msg The Success Message to be displayed
  */
-export function WriteSuccess(msg: string) {
-    console.log(logSymbols.success, chalk.greenBright.bold("[ NICE ]"), msg);
+export function writeSuccess(msg: string) {
+    'use strict';
+    console.log(logSymbols.success, chalk.greenBright.bold('[ NICE ]'), msg);
 }
 
 /**
@@ -71,6 +76,7 @@ export function WriteSuccess(msg: string) {
  * @description Displays Custom Request Message to Console
  * @param msg The Request Message to be displayed
  */
-export function WriteRequest(msg: string) {
-    console.log(circleQuestionMark, chalk.magentaBright.bold("[ IREQ ]"), msg);
+export function writeRequest(msg: string) {
+    'use strict';
+    console.log(circleQuestionMark, chalk.magentaBright.bold('[ IREQ ]'), msg);
 }
